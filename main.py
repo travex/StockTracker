e8de2b599b6fc63b225e2c421e84b495e66a33f0
HemanthK <hemanth.kanner@gmail.com>
2021-03-06 19:14:40 +0100

    load keys

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import pandas as pd
 import sys
 import ast
 import json
+import yaml
 
-
-def random_function():
-    print("Blublue")
-
+def load_keys(filename):
+    with open(filename) as f:
+        keys = yaml.safe_load(f)
+    return keys
 
 if __name__ == '__main__':
-    random_function()
-    print("Hello world")
-    print(rq.__version__)
-    print(sys.version)
+    TwitterKeys = load_keys("twitterAPIKeys.yaml")
+    print(TwitterKeys['BearerToken'])

f67e0b00f0b6742630993502966a0d456bb52498
Joshua Thomas <jbt.josh@gmail.com>
2021-03-06 12:39:28 -0500

    boue blue blue

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import requests as rq
 import pandas as pd
 import sys
 import ast
+import json
+
+
+def random_function():
+    print("Blublue")
+
+
 if __name__ == '__main__':
+    random_function()
     print("Hello world")
     print(rq.__version__)
     print(sys.version)
